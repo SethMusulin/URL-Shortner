ceaac54af96cbe8a01b813bb4043c26c3063429d
Bebe Peng <bebepeng@gmail.com>
2014-03-28 16:27:54 -0600

    Adjusting spec to test for worst edge case.
    Signed-off-by: Seth Musulin <sethmusulin@gmail.com>

--- spec/url_app_spec.rb
+++ spec/url_app_spec.rb
@@ -7,12 +7,13 @@ Capybara.app = UrlApp
 feature "URL Shortener" do
   scenario "user shortenes a URL" do
     visit "/"
-    fill_in "Enter the URL you would like to &quot shorten &quot", :with => "http://www.google.com/"
+    domain = current_url
+    fill_in "Enter the URL you would like to &quot shorten &quot", :with => "google.com"
     click_on "Shorten"
 
-    expect(page).to have_content "http://www.google.com/"
-    expect(page).to have_content "/1"
+    expect(page).to have_content "google.com"
+    expect(page).to have_content "#{domain}/"/\d/
     click_on "short_link"
-    expect(current_url).to eq "http://www.google.com/"
+    expect(current_url).to have_content "google.com"
   end
 end
